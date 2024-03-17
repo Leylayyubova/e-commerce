489ce00e559568eb526965578a15c07383a0bebc
Leyla Ayyubova <102313083+Leylayyubova@users.noreply.github.com>
2024-03-17 21:00:16 +0400

    e-commerce

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper(".mySwiper", {
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+        },
+    });
+});
